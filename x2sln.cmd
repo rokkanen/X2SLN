c78cb4893f25992f02084c2bc9b440bca1f1aecb
Stéphane ROKKANEN <rokkanen@ro2K.com>
2018-03-12 19:03:28 +0100

    correction commentaires

--- x2sln.cmd
+++ x2sln.cmd
@@ -1,6 +1,6 @@
 @echo off
-#1 - Generate createnetcore.cmd via XSLT
+REM 1 - Generate createnetcore.cmd via XSLT
 powershell -file xslt.ps1 %1 SolutionModel2cmd.xsl createnetcore.cmd
-#2 - Execute 
+REM 2 - Execute 
 createnetcore.cmd %2
 
